366f5e6733a02f8a202ca8aad7202ebc38593dbd
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2025-04-05 18:49:45 +0200

    fixed GA var

--- src/app/lib/gtag.ts
+++ src/app/lib/gtag.ts
@@ -5,6 +5,7 @@ export const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
+  if (typeof window.gtag !== 'function') return;
   window.gtag('config', GA_TRACKING_ID as string, {
     page_path: url,
   });
@@ -15,6 +16,7 @@ export const event = (
   action: Gtag.EventNames,
   { event_category, event_label, value }: Gtag.EventParams,
 ) => {
+  if (typeof window.gtag !== 'function') return;
   window.gtag('event', action, {
     event_category,
     event_label,
